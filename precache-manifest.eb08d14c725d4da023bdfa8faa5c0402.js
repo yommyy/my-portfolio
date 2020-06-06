89f2b14e4f5388e2f5259f1ee75cba869a56c792
Doinkyyy <hayagura2698@gmail.com>
2020-06-06 20:32:06 +0800

    Updates

---
+++ precache-manifest.eb08d14c725d4da023bdfa8faa5c0402.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c0b5587cebe7b37f15fc312adb316902",
+    "url": "/my-portfolio/index.html"
+  },
+  {
+    "revision": "9d9b44c34af0c6afc7b3",
+    "url": "/my-portfolio/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "c9366eb6d089f2c126be",
+    "url": "/my-portfolio/static/js/2.26d0daa6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/my-portfolio/static/js/2.26d0daa6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9d9b44c34af0c6afc7b3",
+    "url": "/my-portfolio/static/js/main.54fc776e.chunk.js"
+  },
+  {
+    "revision": "170f45050b91094c3a2e",
+    "url": "/my-portfolio/static/js/runtime-main.354f34ae.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/my-portfolio/static/media/logo.5d5d9eef.svg"
+  }
+]);
